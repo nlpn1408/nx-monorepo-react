29bf4a24a539dd17ec6315ebc7c1f8e91cf83798
nlpn1408 <96246678+nlpn1408@users.noreply.github.com>
2025-08-15 16:01:47 +0700

    update admin console

--- apps/admin-console/src/app/app.tsx
+++ apps/admin-console/src/app/app.tsx
@@ -5,7 +5,7 @@ export function App() {
   return (
     <div>
       <h1>Admin Console</h1>
-      <p>Welcome to the Admin Console!</p>
+      <p>Welcome to the Admin Console! asdasdasdsd@S</p>
       {/* Uncomment the line below to use CSS modules */}
       {/* <div className={styles.container}>This is a styled container.</div> */}
       {/* Add your components and routes here */}
